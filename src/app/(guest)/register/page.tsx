8850cc63a1bf58ad06f885046d34f761f3e4c24c
Godito, Marjon H. <godito0770@gmanmi.com>
2025-01-13 14:40:34 +0800

    Fix register page

--- src/app/(guest)/register/page.tsx
+++ src/app/(guest)/register/page.tsx
@@ -12,7 +12,7 @@ export default function Register() {
             <div className="w-96 bg-base-200 p-4 rounded-lg shadow-md">
                 <div className="text-2xl font-bold">Register</div>
                 <div className="flex flex-col gap-4 mt-4">
-                    <Input placeholder="Email" type="email"/>
+                    <Input placeholder="Email" type="email" name="email"/>
                     <Input placeholder="Username" name="username"/>
                     <Input placeholder="Password" type="password" name="password"/>
                     <Input placeholder="Confirm Password" type="password" name="confirm_password"/>

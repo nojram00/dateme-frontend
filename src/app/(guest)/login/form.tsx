be91a81c943e8fd249df979737d88b51c96ee060
Godito, Marjon H. <godito0770@gmanmi.com>
2025-01-20 20:14:47 +0800

    Okie shits 3

--- src/app/(guest)/login/form.tsx
+++ src/app/(guest)/login/form.tsx
@@ -11,14 +11,14 @@ export default function LoginForm() {
     return (
         <form className="flex flex-col gap-4 mt-4" action={action}>
 
-            <Input placeholder="Username" name="username"/>
-            <Input placeholder="Password" type="password" name="password"/>
+            <Input placeholder="Username" name="username" disabled={pending}/>
+            <Input placeholder="Password" type="password" name="password" disabled={pending}/>
 
             <span className="text-center">No account yet? <Link className="underline hover:text-gray-300"
                                                                 href="/register">Register</Link></span>
 
             { prevState?.error && <div className="text-red-500 p-2 border rounded-sm">{prevState.error}</div> }
-            <button disabled={pending} className="bg-primary text-white p-2 rounded-lg hover:bg-primary-dark">Login</button>
+            <button disabled={pending} className="bg-primary text-white p-2 rounded-lg hover:bg-primary-dark">{ !pending ? "Login" : "Logging In..." }</button>
         </form>
     )
 
